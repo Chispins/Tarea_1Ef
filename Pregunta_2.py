485320397f7516a0474fcca2fdc3dde8437f0ce2
Thinkpad <sercontrer@fen.uchile.cl>
2026-03-31 11:35:13 -0300

    Trivial Changes

--- Pregunta_2.py
+++ Pregunta_2.py
@@ -1,4 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+np.random.seed(42)
 
 data = pd.read_excel('bbdd_Tarea1.xlsx')
 
@@ -13,7 +17,6 @@ x2 = np.array(x2)
 x1 = x1.reshape((n, 1))
 x2 = x2.reshape((n, 1))
 
-
 X = np.concatenate([np.ones((n, 1)), x1, x2], axis=1)
 X_sin_constante = np.concatenate((x1, x2), axis=1)
 
@@ -24,24 +27,34 @@ Betas = Betas.reshape((3, 1))
 Betas_sin_constante = np.array((B1, B2))
 Betas_sin_constante = Betas_sin_constante.reshape((2, 1))
 
-error = np.random.normal(0, 0.0625, size=(n, 1))
+sigma = 0.0625
+error = np.random.normal(0, sigma, size=(n, 1))
+
 y_real = X @ Betas
 y_observado = X @ Betas + error  # Y con constante
 
 Beta_gorro = np.linalg.inv(X.T @ X)@X.T @ y_observado
 Beta_gorro_sc = np.linalg.inv(X_sin_constante.T @ X_sin_constante)@X_sin_constante.T @ y_observado
 
+
+parte_1_sc = np.linalg.inv(X_sin_constante.T @ X_sin_constante) @X_sin_constante.T
+parte_1 =np.linalg.inv(X.T @ X) @ X.T
 beta_gorro_MC = []
 beta_gorro_sc_MC = []
 for i in range(1000):
-    itero = y_real + np.random.normal(0, 0.0625, size=(n, 1))
-    itero_beta_gorro = np.linalg.inv(X.T @ X) @ X.T @ itero
-    itero_beta_gorro_sc = np.linalg.inv(X_sin_constante.T @ X_sin_constante) @X_sin_constante.T @ itero
+    itero = y_real + np.random.normal(0, sigma, size=(n, 1))
+    itero_beta_gorro = parte_1 @ itero
+    itero_beta_gorro_sc = parte_1_sc @ itero
 
     beta_gorro_MC.append(itero_beta_gorro)
     beta_gorro_sc_MC.append(itero_beta_gorro_sc)
+
+
 beta_gorro_MC_np = np.array(beta_gorro_MC)
 beta_gorro_sc_MC_np = np.array(beta_gorro_sc_MC)
 
 Betas_MC_sc = [beta_gorro_sc_MC_np[:, 0].mean(),beta_gorro_sc_MC_np[:, 1].mean()]
+Betas_MC_sc_std = [beta_gorro_sc_MC_np[:, 0].std(),beta_gorro_sc_MC_np[:, 1].std()]
 Betas_MC = [beta_gorro_MC_np[:, 0].mean(), beta_gorro_MC_np[:, 1].mean(), beta_gorro_MC_np[:, 2].mean()]
+Betas_MC_std = [beta_gorro_MC_np[:, 0].std(), beta_gorro_MC_np[:, 1].std(), beta_gorro_MC_np[:, 2].std()]
+
